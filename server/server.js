c59e8932b0d87c0255cf6cfb910b77560afa5d22
Dean Long <deandamon12@gmail.com>
2023-03-22 01:05:11 +1100

    change prompt parameters

--- server/server.js
+++ server/server.js
@@ -29,7 +29,7 @@ app.post('/',async(req,res) => {
         const response = await openai.createChatCompletion(
             {
                 model: "gpt-3.5-turbo",
-                prompt: `${prompt}`,
+                messages: `${prompt}`,
                 temperature: 0, // Higher values means the model will take more risks.
                 max_tokens: 4096, // The maximum number of tokens to generate in the completion. Most models have a context length of 2048 tokens (except for the newest models, which support 4096).
                 top_p: 1, // alternative to sampling with temperature, called nucleus sampling
@@ -45,7 +45,7 @@ app.post('/',async(req,res) => {
             // presence_penalty=0
         );
         res.status(200).send({
-            bot: response.data.choices[0].text
+            bot: response.data.choices[0].message.content
         })
     } catch (error) {
         console.log(error);
