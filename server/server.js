90f1d6b3ff976536f7b60f0c66f2b803cebf8985
Dean Long <deandamon12@gmail.com>
2023-03-21 23:48:40 +1100

    update model to gpt3.5 turbo

--- server/server.js
+++ server/server.js
@@ -28,7 +28,7 @@ app.post('/',async(req,res) => {
         const prompt = req.body.prompt;
         const response = await openai.createCompletion(
             {
-                model: "text-davinci-003",
+                model: "gpt-3.5-turbo",
                 prompt: `${prompt}`,
                 temperature: 0, // Higher values means the model will take more risks.
                 max_tokens: 3000, // The maximum number of tokens to generate in the completion. Most models have a context length of 2048 tokens (except for the newest models, which support 4096).
@@ -36,7 +36,7 @@ app.post('/',async(req,res) => {
                 frequency_penalty: 0.5, // Number between -2.0 and 2.0. Positive values penalize new tokens based on their existing frequency in the text so far, decreasing the model's likelihood to repeat the same line verbatim.
                 presence_penalty: 0, // Number between -2.0 and 2.0. Positive values penalize new tokens based on whether they appear in the text so far, increasing the model's likelihood to talk about new topics.
               }
-            // model="text-davinci-003",
+            // model="gpt-3.5-turbo",
             // prompt= `${prompt}`,
             // temperature=0.3,
             // max_tokens=3000,
