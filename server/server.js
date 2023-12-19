03b08910d361b00b73006f27936bdec61bab1693
Dean Long <88417254+deanlongio@users.noreply.github.com>
2023-12-20 00:32:33 +1100

    Update server.js

--- server/server.js
+++ server/server.js
@@ -31,6 +31,12 @@ app.post('/',async(req,res) => {
                 prompt: `Based on https://www.deanlong.io/, act like an assistant of this website,${prompt}`,
                 temperature: 0.5, // Higher values means the model will take more risks.
                 max_tokens: 3000, // The maximum number of tokens to generate in the completion. Most models have a context length of 2048 tokens (except for the newest models, which support 4096).
+                messages=[
+                    {
+                      "role": "user",
+                      "content": "Based on https://www.deanlong.io/, act like an assistant of this website"
+                    }
+                  ],
                 top_p: 1, // alternative to sampling with temperature, called nucleus sampling
                 frequency_penalty: 0.8, // Number between -2.0 and 2.0. Positive values penalize new tokens based on their existing frequency in the text so far, decreasing the model's likelihood to repeat the same line verbatim.
                 presence_penalty: 0.5, // Number between -2.0 and 2.0. Positive values penalize new tokens based on whether they appear in the text so far, increasing the model's likelihood to talk about new topics.
