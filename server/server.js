c27565a7e0c166f4b0be65dcd652bb86f97efba7
Dean Long <88417254+deanlongio@users.noreply.github.com>
2023-12-20 00:45:48 +1100

    Update server.js

--- server/server.js
+++ server/server.js
@@ -25,18 +25,12 @@ res.status(200).send({
 app.post('/',async(req,res) => {
     try{
         const prompt = req.body.prompt;
-        const response = await openai.chat.completions.create(
+        const response = await openai.completions.create(
             {
-                model: "gpt-3.5-turbo",
+                model: "gpt-3.5-turbo-instruct-0914",
                 prompt: `Based on https://www.deanlong.io/, act like an assistant of this website,${prompt}`,
                 temperature: 0.5, // Higher values means the model will take more risks.
                 max_tokens: 3000, // The maximum number of tokens to generate in the completion. Most models have a context length of 2048 tokens (except for the newest models, which support 4096).
-                messages:[
-                    {
-                      "role": "user",
-                      "content": "Based on https://www.deanlong.io/, act like an assistant of this website"
-                    }
-                  ],
                 top_p: 1, // alternative to sampling with temperature, called nucleus sampling
                 frequency_penalty: 0.8, // Number between -2.0 and 2.0. Positive values penalize new tokens based on their existing frequency in the text so far, decreasing the model's likelihood to repeat the same line verbatim.
                 presence_penalty: 0.5, // Number between -2.0 and 2.0. Positive values penalize new tokens based on whether they appear in the text so far, increasing the model's likelihood to talk about new topics.
